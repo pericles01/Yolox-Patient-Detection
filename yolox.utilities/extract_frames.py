4483db8d86686d8f075685e090f2f911050c0b2f
junior pericles <juniorprcls@gmail.com>
2022-09-17 06:49:09 +0200

    updated extract_frames algorithm

--- yolox.utilities/extract_frames.py
+++ yolox.utilities/extract_frames.py
@@ -1,78 +1,109 @@
-# Importing all necessary libraries
-import cv2
+import numpy as np
 import os
+import cv2
+import logging
+import argparse
 
-# Read the video from specified path
-video_path = "./mock_or_ai_1_view_one_chunk0.mp4"
-#data path
-export_path = "./test_images"
-#rescale size if needed
-image_size = (608, 608)
+def collect_videos(path):
 
+        files = []
 
-def check_path(path):
-    if not os.path.exists(path):
-        print('video path is not exist. Please create the folder path for videos and place videos there!')
-        exit(1)
+        #check the path
+        if (os.path.isdir(path)):
+            
+            for video in sorted(os.listdir(path)):
+                complete_path = os.path.join(path, video)
+                if complete_path.upper().endswith((".MP4", ".mp4")):
+                    
+                    logging.info('* collecting video: ' + video + ' from folder: ' + path)
+                    files.extend([{
+                        'path': complete_path,
+                        'name': os.path.splitext(video)[0],
+                        'hour_folder': path
+                    } 
+                    ])
+        elif (os.path.isfile(path)):
+            if path.upper().endswith((".MP4", ".mp4")):
+            
+                logging.info('* collecting video: ' + path)
+                root = os.path.splitext(path)[0]
+                files.extend([{
+                        'path': path,
+                        'name': root.split('/')[-1],
+                    } 
+                    ])
+            else:
+                raise ("File is not a video. Must End with: .MP4, .mp4")
 
-def extract_frames(video_path, frames_dir, resize=True, start=-1, end=-1, every=1):
-    """
-    Extract frames from a video using OpenCVs VideoCapture
-    :param video_path: path of the video
-    :param frames_dir: the directory to save the frames
-    :param start: start frame
-    :param end: end frame
-    :param every: frame spacing
-    :return: count of images saved
-    """
-    
-    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
-    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
-
-    check_path(frames_dir)
-    check_path(video_path)
-
-    capture = cv2.VideoCapture(video_path)  # open the video using OpenCV
-
-    if start < 0:  # if start isn't specified lets assume 0
-        start = 0
-    if end < 0:  # if end isn't specified assume the end of the video
-        end = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    capture.set(1, start)  # set the starting frame of the capture
-    frame = start  # keep track of which frame we are up to, starting from start
-    while_safety = 0  # a safety counter to ensure we don't enter an infinite while loop (hopefully we won't need it)
-    saved_count = 0  # a count of how many frames we have saved
-
-    while frame < end:  # lets loop through the frames until the end
-
-        _, image = capture.read()  # read an image from the capture
-
-        if while_safety > 500:  # break the while if our safety maxs out at 500
-            break
-
-        # sometimes OpenCV reads None's during a video, in which case we want to just skip
-        if image is None:  # if we get a bad return flag or the image we read is None, lets not save
-            while_safety += 1  # add 1 to our while safety, since we skip before incrementing our frame variable
-            continue  # skip
-
-        if frame % every == 0:  # if this is a frame we want to write out based on the 'every' argument
-            while_safety = 0  # reset the safety count
-            save_path = os.path.join(frames_dir, "{:10d}.jpg".format(frame))  # create the save path
-            print ('Creating...' + save_path)
-            if resize:
-                image = cv2.resize(image, image_size)
+        else:
         
-            cv2.imwrite(save_path, image)  # save the extracted image
-            saved_count += 1  # increment our counter by one
+            raise ('Path should be a directory or a video file')
 
-        frame += 1  # increment our frame count
+    
+        return files
 
-    capture.release()  # after the while has finished close the capture
-    cv2.destroyAllWindows()
+def main():
 
-    print("{:5d} Images saved".format(saved_count)) # print the count of the images we saved
+    for file in collect_videos(args.file):
+        #with open(file['path'], "rb") as video:
+    
+            capture = cv2.VideoCapture(file['path'])
+            capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
+            fps = int(capture.get(cv2.CAP_PROP_FPS))
+            frames_number = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+            every = fps // args.fps
+            index = 0
+            saved_count = 0
+            while index < frames_number:
+                #Capture frame-by-frame
+                ret, frame = capture.read()
+                if not ret:
+                    break           
+                if frame is None:
+                    continue
+                
+                if index % every == 0:
+                    # arr = np.ndarray((frame.size, 1), buffer=frame, dtype=np.uint8)
+                    # print(f"frame: {index} frame shape: {arr.shape} sent to pipeline")
+                    save_path = os.path.join(args.output, "{:3d}.jpg".format(saved_count))  # create the save path
+                    print ('Creating...' + save_path)
+                    if args.resize_dim:
+                        if isinstance(args.resize_dim, int):
+                            dim = [args.resize_dim, args.resize_dim]
+                        elif isinstance(args.resize_dim, list):
+                            if len(args.resize_dim) == 2:
+                                dim = args.resize_dim
+                            else:
+                                raise("dimension list must be in a form [M, N]")
 
-if __name__ == "__main__":
-    #start=-1, end=-1,
-    extract_frames(video_path=video_path, frames_dir=export_path, resize=True, every=15)
+                        frame = cv2.resize(frame, dim)
+        
+                    cv2.imwrite(save_path, frame)  # save the extracted image
+                    saved_count += 1
+
+                index += 1
+                if index == 100:
+                    break
+
+            logging.info('Video name * ' + file['name'] +' * with FPS: '+str(fps)+ '. Need fps: '+str(args.fps)+' sending every '+str(every)+' frames')
+            logging.info('Processed ' +str(saved_count) + ' frames from ' + str(index))
+
+            # When everything done, release the video capture object
+            capture.release()
+            # Closes all the frames
+            cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-file', type=str, help='Video path')
+    parser.add_argument('-fps', type=int, default= None, help='set fps number for extraction')
+    parser.add_argument('-resize_dim', type=(int,list), help='dimension to resize extracted frames. Input a single Integer for N*N dimension or input a dimension list [N,M]')
+    parser.add_argument('-output', type=str, help='output path to save extracted frames')
+    args = parser.parse_args()
+
+    #files = collect_videos(args.file)
+    main()
